29c1051efc73054b7bcbe676bf6f690b6427cd63
KaterynaLikhvar <likhvar.ks@gmail.com>
2021-11-13 15:27:00 +0200

    feat: add sidebar layout

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -2,39 +2,37 @@ console.log('file 1');
 const daysBtn = document.querySelector('.days__button');
 const click = document.getElementById("list-items");
 const burgerBtn = document.querySelector('.header__menu');
-const navbar = document.querySelector('.hamburger-menu');
+const sidebar = document.querySelector('.sidebar');
 const lang = document.querySelector('.lang-container');
 const langMenu = document.getElementById('lang__list-items');
 const vectorDown = document.querySelector('.vector-down');
+const coverElem = document.getElementById('cover');
+const closeBtn = document.querySelector('.sidebar__close-btn');
 //console.log(vectorUp)
 
-
-daysBtn.addEventListener('click', event => {
-  if(click.classList.contains('hide')) {
-    click.classList.remove('hide');
-    click.classList.add('show');
- } else {
-   click.classList.add('hide');
- }
+burgerBtn.addEventListener('click', () => {
+  document.body.classList.add('notScrollable');
+  coverElem.classList.remove('hidden');
+  sidebar.classList.remove('hidden');
 });
 
-burgerBtn.addEventListener('click', () => {
-  if(navbar.classList.contains('hide')) {
-    navbar.classList.remove('hide');
-    navbar.classList.add('show');
- } else {
-   navbar.classList.add('hide');
- }
+coverElem.addEventListener('click', () => {
+  document.body.classList.remove('notScrollable');
+  coverElem.classList.add('hidden');
+  sidebar.classList.add('hidden');
+});
+closeBtn.addEventListener('click', () => {
+  document.body.classList.remove('notScrollable');
+  coverElem.classList.add('hidden');
+  sidebar.classList.add('hidden');
 });
 
 lang.addEventListener('click', () => {
-  if(langMenu.classList.contains('hide')) {
-    langMenu.classList.remove('hide');
-    langMenu.classList.add('show');
+  if(langMenu.classList.contains('hidden')) {
+    langMenu.classList.remove('hidden');
     vectorDown.classList.add('vector-up');
  } else {
-  langMenu.classList.remove('show');
-  langMenu.classList.add('hide');
+  langMenu.classList.add('hidden');
   vectorDown.classList.remove('vector-up');
  }
 })
